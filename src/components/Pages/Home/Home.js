92d98c86af7c51b5f076afa24408f74642070c1e
kevin <kevin13348@gmail.com>
2023-05-26 13:38:28 -0300

    styled the table

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,8 +1,7 @@
 import styles from './Home.module.css'
 import Title from '../../Title/Title'
 import { NavLink } from 'react-router-dom'
-import TableHead from '../../Table/TableHead/TableHead'
-import TableBody from '../../Table/TableBody/TableBody'
+import EquipmentTable from '../../Table/EquipmentTable/EquipmentTable'
 
 const Home = () => {
   return (
@@ -11,24 +10,7 @@ const Home = () => {
         <Title>Equipamentos</Title>
         <NavLink to={'/equipamentos/new'} className={`button-default`}><i className='pi pi-plus'></i></NavLink>
       </header>
-      <table className={styles.equipTable}>
-        <TableHead>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Tipo</th>
-          <th>Descrição</th>
-          <th>Status</th>
-        </TableHead>
-        <TableBody>
-          <tr>
-            <th>123</th>
-            <td>Tecrado</td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </TableBody>
-      </table>
+      <EquipmentTable/>
     </section>
   )
 }
---
+++ src/components/Table/EquipmentTable/EquipmentTable.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './EquipmentTable.module.css'
+import tableStyles from '../Table.module.css'
+
+const EquipmentTable = () => {
+  return (
+      <table className={`${styles.equipTable}`}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Tipo</th>
+            <th>Descrição</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>123</th>
+            <td>Tecrado</td>
+            <td>ala</td>
+            <td>ala</td>
+            <td>ala</td>
+          </tr>
+          <tr>
+            <th>123</th>
+            <td>Tecrado</td>
+            <td>ala</td>
+            <td>ala</td>
+            <td>ala</td>
+          </tr>
+        </tbody>
+      </table>
+  )
+}
+
+export default EquipmentTable
