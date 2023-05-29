77eeeb268eec0a3c7e0f06f9e2d70bf92f747575
kevin <kevin13348@gmail.com>
2023-05-29 11:47:37 -0300

    created loading state

--- src/components/Table/TipoTable/TipoTable.js
+++ src/components/Table/TipoTable/TipoTable.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import '../Table.css'
 import styles from './TipoTable.module.css'
+import Loading from '../../Helper/Loading'
 
 const TipoTable = () => {
   const [ tipos, setTipos ] = useState([])
+  const [ loading, setLoading ] = useState(null)
 
   useEffect(() => {
+    setLoading(true)
     fetch('https://api-pi-2on3.onrender.com/tipos')
     .then(res => res.json())
-    .then(json => setTipos(json))
+    .then(json => {setTipos(json); setLoading(false)})
   }, [])
 
   return (
@@ -20,7 +23,12 @@ const TipoTable = () => {
         </tr>
       </thead>
       <tbody>
-        {tipos.map( tipo => (
+        { loading ? (
+          <tr>
+            <th><Loading/></th>
+            <td><Loading/></td>
+          </tr>
+        ) : tipos.map( tipo => (
           <tr key={tipo.id_tipo}>
             <th>{tipo.id_tipo}</th>
             <td>{tipo.nome}</td>
--- src/components/Table/ZonaTable/ZonaTable.js
+++ src/components/Table/ZonaTable/ZonaTable.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import '../Table.css'
 import styles from './ZonaTable.module.css'
+import Loading from '../../Helper/Loading'
 
 const ZonaTable = () => {
   const [ zonas, setZonas ] = useState([])
+  const [ loading, setLoading ] = useState(null)
 
   useEffect(() => {
+    setLoading(true)
     fetch('https://api-pi-2on3.onrender.com/zonas')
     .then(res => res.json())
-    .then(json => setZonas(json))
+    .then(json => {setZonas(json); setLoading(false)})
   }, [])
 
   return (
@@ -21,7 +24,13 @@ const ZonaTable = () => {
         </tr>
       </thead>
       <tbody>
-        {zonas.map( zona => (
+        {loading ? (
+          <tr>
+            <th><Loading/></th>
+            <td><Loading/></td>
+            <td><Loading/></td>
+          </tr>
+        ) : zonas.map( zona => (
           <tr key={zona.id_zona}>
             <th>{zona.id_zona}</th>
             <td>{zona.nome}</td>
